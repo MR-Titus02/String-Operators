e399d9b4f85d44ce263c867d0509e6dfee5b1751
MR-Titus02 <titusroxsan@gmail.com>
2025-03-19 20:21:29 +0530

    project2 fixed again

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
 function concat() {
-    let text1 = document.getElementById('text1').value;
-    text1 = text1.toUpperCase();
-    let text2 = document.getElementById('text2').value;
-    text2 = text2.toLowerCase();
-    let text3 = text1.concat(text2);
-    document.getElementById('result').innerHTML = text3;
-    let length1 = text1.length;
-    let length2 = text2.length;
-    document.getElementById('count1').innerHTML = "The length of first string is " + length1;
-    document.getElementById('count2').innerHTML = "The length of second string is " + length2;
+  // for concat
+  let text1 = document.getElementById("text1").value;
+  let text2 = document.getElementById("text2").value;
+  let text3 = text1.concat(text2);
+  document.getElementById("concat").innerHTML = text3;
 
+  // for length
+  let length1 = text1.length;
+  let length2 = text2.length;
+  document.getElementById("count1").innerHTML = length1;
+  document.getElementById("count2").innerHTML = length2;
+
+  // for upper & lower case
+  text1 = text1.toUpperCase();
+  text2 = text2.toLowerCase();
+  document.getElementById("upper").innerHTML = text1;
+  document.getElementById("lower").innerHTML = text2;
 }
-
-
